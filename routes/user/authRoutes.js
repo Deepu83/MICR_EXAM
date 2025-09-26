25edee9eab556df754d8a5178ef0b42f7eda027a
Saurabh-Techie <sk2009025@gmail.com>
2025-09-26 10:12:02 +0530

    profileUpdated change

--- routes/user/authRoutes.js
+++ routes/user/authRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { register, login } from "../../controllers/user/authController.js";
+import { register, login , updateProfile} from "../../controllers/user/authController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.put("/update-profile/:userId", updateProfile);
 
 export default router;
